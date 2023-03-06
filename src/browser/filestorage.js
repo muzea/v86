33002ca2a71a1548e22ca0188fd5e5b27914b24d
MuYu <mr.muzea@gmail.com>
2023-03-06 15:02:16 +0000

    feat: support pack v3 format

--- src/browser/filestorage.js
+++ src/browser/filestorage.js
@@ -1,7 +1,7 @@
 "use strict";
 
 /** @interface */
-function FileStorageInterface() { }
+function FileStorageInterface() {}
 
 /**
  * Read a portion of a file.
@@ -10,7 +10,7 @@ function FileStorageInterface() { }
  * @param {number} count
  * @return {!Promise<Uint8Array>} null if file does not exist.
  */
-FileStorageInterface.prototype.read = function (sha256sum, offset, count) { };
+FileStorageInterface.prototype.read = function (sha256sum, offset, count) {};
 
 /**
  * Add a read-only file to the filestorage.
@@ -18,14 +18,14 @@ FileStorageInterface.prototype.read = function (sha256sum, offset, count) { };
  * @param {!Uint8Array} data
  * @return {!Promise}
  */
-FileStorageInterface.prototype.cache = function (sha256sum, data) { };
+FileStorageInterface.prototype.cache = function (sha256sum, data) {};
 
 /**
  * Call this when the file won't be used soon, e.g. when a file closes or when this immutable
  * version is already out of date. It is used to help prevent accumulation of unused files in
  * memory in the long run for some FileStorage mediums.
  */
-FileStorageInterface.prototype.uncache = function (sha256sum) { };
+FileStorageInterface.prototype.uncache = function (sha256sum) {};
 
 /**
  * @constructor
@@ -92,11 +92,11 @@ function ServerFileStorageWrapper(file_storage, baseurl) {
 ServerFileStorageWrapper.prototype.load_from_server = function (sha256sum) {
     return new Promise((resolve, reject) => {
         v86util.load_file(this.baseurl + sha256sum, {
-            done: async buffer => {
+            done: async (buffer) => {
                 const data = new Uint8Array(buffer);
                 await this.cache(sha256sum, data);
                 resolve(data);
-            }
+            },
         });
     });
 };
@@ -131,257 +131,32 @@ ServerFileStorageWrapper.prototype.uncache = function (sha256sum) {
     this.storage.uncache(sha256sum);
 };
 
+let fsCache = caches.open("v86-fs-request");
 
-// copy from https://www.npmjs.com/package/@kvs/indexeddb
-function invariant(condition, message) {
-    if (condition) {
-        return;
-    }
-    throw new Error(message);
+/**
+ *
+ * @param {string} url
+ * @returns {Promise<Response>}
+ */
+async function fetchWithCache(url) {
+    const cacheIns = await fsCache;
+    const maybeResult = await cacheIns.match(url);
+    if (maybeResult) return maybeResult;
+
+    const result = await fetch(url);
+
+    await cacheIns.put(url, result.clone());
+    return result;
 }
-const openDB = ({ name, version, tableName, onUpgrade }) => {
-    return new Promise((resolve, reject) => {
-        const openRequest = indexedDB.open(name, version);
-        openRequest.onupgradeneeded = function (event) {
-            var _a;
-            // IndexedDB has oldVersion and newVersion is native properties
-            const oldVersion = event.oldVersion;
-            const newVersion = (_a = event.newVersion) !== null && _a !== void 0 ? _a : version;
-            const database = openRequest.result;
-            try {
-                // create table at first time
-                if (!newVersion || newVersion <= 1) {
-                    database.createObjectStore(tableName);
-                }
-            }
-            catch (e) {
-                reject(e);
-            }
-            // for drop instance
-            // https://github.com/w3c/IndexedDB/issues/78
-            // https://www.w3.org/TR/IndexedDB/#introduction
-            database.onversionchange = () => {
-                database.close();
-            };
-            // @ts-expect-error: target should be existed
-            event.target.transaction.oncomplete = async () => {
-                try {
-                    await onUpgrade({
-                        oldVersion,
-                        newVersion,
-                        database
-                    });
-                    return resolve(database);
-                }
-                catch (error) {
-                    return reject(error);
-                }
-            };
-        };
-        openRequest.onblocked = () => {
-            reject(openRequest.error);
-        };
-        openRequest.onerror = function () {
-            reject(openRequest.error);
-        };
-        openRequest.onsuccess = function () {
-            const db = openRequest.result;
-            resolve(db);
-        };
-    });
-};
-const dropInstance = (database, databaseName) => {
-    return new Promise((resolve, reject) => {
-        database.close();
-        const request = indexedDB.deleteDatabase(databaseName);
-        request.onupgradeneeded = (event) => {
-            event.preventDefault();
-            resolve();
-        };
-        request.onblocked = () => {
-            reject(request.error);
-        };
-        request.onerror = function () {
-            reject(request.error);
-        };
-        request.onsuccess = function () {
-            resolve();
-        };
-    });
-};
-const getItem = (database, tableName, key) => {
-    return new Promise((resolve, reject) => {
-        const transaction = database.transaction(tableName, "readonly");
-        const objectStore = transaction.objectStore(tableName);
-        const request = objectStore.get(String(key));
-        request.onsuccess = () => {
-            resolve(request.result);
-        };
-        request.onerror = () => {
-            reject(request.error);
-        };
-    });
-};
-const hasItem = async (database, tableName, key) => {
-    return new Promise((resolve, reject) => {
-        const transaction = database.transaction(tableName, "readonly");
-        const objectStore = transaction.objectStore(tableName);
-        const request = objectStore.count(String(key));
-        request.onsuccess = () => {
-            resolve(request.result !== 0);
-        };
-        request.onerror = () => {
-            reject(request.error);
-        };
-    });
-};
-const setItem = async (database, tableName, key, value) => {
-    // If the value is undefined, delete the key
-    // This behavior aim to align localStorage implementation
-    if (value === undefined) {
-        await deleteItem(database, tableName, key);
-        return;
-    }
-    return new Promise((resolve, reject) => {
-        const transaction = database.transaction(tableName, "readwrite");
-        const objectStore = transaction.objectStore(tableName);
-        const request = objectStore.put(value, String(key));
-        transaction.oncomplete = () => {
-            resolve();
-        };
-        transaction.onabort = () => {
-            reject(request.error ? request.error : transaction.error);
-        };
-        transaction.onerror = () => {
-            reject(request.error ? request.error : transaction.error);
-        };
-    });
-};
-const deleteItem = async (database, tableName, key) => {
-    return new Promise((resolve, reject) => {
-        const transaction = database.transaction(tableName, "readwrite");
-        const objectStore = transaction.objectStore(tableName);
-        const request = objectStore.delete(String(key));
-        transaction.oncomplete = () => {
-            resolve();
-        };
-        transaction.onabort = () => {
-            reject(request.error ? request.error : transaction.error);
-        };
-        transaction.onerror = () => {
-            reject(request.error ? request.error : transaction.error);
-        };
-    });
-};
-const clearItems = async (database, tableName) => {
-    return new Promise((resolve, reject) => {
-        const transaction = database.transaction(tableName, "readwrite");
-        const objectStore = transaction.objectStore(tableName);
-        const request = objectStore.clear();
-        transaction.oncomplete = () => {
-            resolve();
-        };
-        transaction.onabort = () => {
-            reject(request.error ? request.error : transaction.error);
-        };
-        transaction.onerror = () => {
-            reject(request.error ? request.error : transaction.error);
-        };
-    });
-};
-const iterator = (database, tableName) => {
-    const handleCursor = (request) => {
-        return new Promise((resolve, reject) => {
-            request.onsuccess = () => {
-                const cursor = request.result;
-                if (!cursor) {
-                    return resolve({
-                        done: true
-                    });
-                }
-                return resolve({
-                    done: false,
-                    value: cursor
-                });
-            };
-            request.onerror = () => {
-                reject(request.error);
-            };
-        });
-    };
-    const transaction = database.transaction(tableName, "readonly");
-    const objectStore = transaction.objectStore(tableName);
-    const request = objectStore.openCursor();
-    return {
-        async next() {
-            const { done, value } = await handleCursor(request);
-            if (!done) {
-                const storageKey = value === null || value === void 0 ? void 0 : value.key;
-                const storageValue = value === null || value === void 0 ? void 0 : value.value;
-                value === null || value === void 0 ? void 0 : value.continue();
-                return { done: false, value: [storageKey, storageValue] };
-            }
-            return { done: true, value: undefined };
-        }
-    };
-};
-const createStore = ({ database, databaseName, tableName }) => {
-    const store = {
-        delete(key) {
-            return deleteItem(database, tableName, key).then(() => true);
-        },
-        get(key) {
-            return getItem(database, tableName, key);
-        },
-        has(key) {
-            return hasItem(database, tableName, key);
-        },
-        set(key, value) {
-            return setItem(database, tableName, key, value).then(() => store);
-        },
-        clear() {
-            return clearItems(database, tableName);
-        },
-        dropInstance() {
-            return dropInstance(database, databaseName);
-        },
-        close() {
-            return Promise.resolve().then(() => {
-                database.close();
-            });
-        },
-        [Symbol.asyncIterator]() {
-            return iterator(database, tableName);
-        },
-        __debug__database__: database
-    };
-    return store;
-};
 
-const kvsIndexedDB = async (options) => {
-    var _a;
-    const { name, version, upgrade, ...indexDBOptions } = options;
-    invariant(typeof name === "string", "name should be string");
-    invariant(typeof version === "number", "version should be number");
-    const tableName = (_a = indexDBOptions.tableName) !== null && _a !== void 0 ? _a : "kvs";
-    const database = await openDB({
-        name,
-        version,
-        tableName,
-        onUpgrade: ({ oldVersion, newVersion, database }) => {
-            if (!upgrade) {
-                return;
-            }
-            return upgrade({
-                kvs: createStore({ database: database, tableName: tableName, databaseName: name }),
-                oldVersion,
-                newVersion
-            });
-        }
-    });
-    return createStore({ database: database, tableName: tableName, databaseName: name });
-};
-
+/**
+ *
+ * @param {string} url
+ * @returns {Promise<ArrayBuffer>}
+ */
+export async function fetchArrayBuffer(url) {
+    return (await fetchWithCache(url)).arrayBuffer();
+}
 
 /**
  * @implements {FileStorageInterface}
@@ -393,23 +168,16 @@ class ServerPackStorageWrapper {
         this.storage = file_storage;
         this.baseurl = baseurl;
         // store promise avoid parallel calls
-        this.fileInfoMap = {};
         this.packMap = {};
-        this.kv = null;
-        if (typeof use_pack === 'object') {
-            this.prefix_length = use_pack.prefix_length || 1;
 
-            if (use_pack.idb_key) {
-                kvsIndexedDB({
-                    name: use_pack.idb_key,
-                    version: 1,
-                }).then(ins => {
-                    this.kv = ins;
-                });
-            }
-        } else {
-            this.prefix_length = 1;
-        }
+        /**
+         * p - pack
+         * The file is in the p-th pack congregation
+         * s - start
+         * l - length
+         * @type {Map<number, { p: number; s: number; l: number }>}
+         */
+        this.fileMap = use_pack.fileMap;
     }
 
     /**
@@ -417,88 +185,23 @@ class ServerPackStorageWrapper {
      * @return {!Promise<Uint8Array>}
      */
     async load_from_server(sha256sum) {
-        console.log('try load file ', sha256sum);
-        // check file map
-        const prefix = sha256sum.substring(0, this.prefix_length);
-        if (!this.fileInfoMap[prefix]) {
-
-            this.fileInfoMap[prefix] = new Promise((resolveMap) => {
-                const resourceUrl = this.baseurl + `${prefix}.map.json`;
-
-                const loadResource = () => {
-                    v86util.load_file(resourceUrl, {
-                        as_json: true,
-                        /**
-                         * @param {Array<number>} resp 
-                         */
-                        done: async resp => {
-                            const infoMap = {};
-                            let i = 0;
-                            let end = resp.length;
-                            let start = 0;
-                            while (i < end) {
-                                const fileEnd = start + resp[i + 1];
-                                infoMap[resp[i]] = {
-                                    start: start,
-                                    end: fileEnd,
-                                }
-                                start = fileEnd;
-
-                                i += 2;
-                            }
-                            this.kv && this.kv.set(resourceUrl, infoMap);
-                            resolveMap(infoMap);
-                        }
-                    });
-                }
-
-                if (this.kv) {
-                    return this.kv.has(resourceUrl).then(hasCache => {
-                        if (hasCache) {
-                            return resolveMap(this.kv.get(resourceUrl));
-                        }
-                        loadResource();
-                    });
-                }
-                loadResource();
-            });
-
-            this.packMap[prefix] = new Promise((resolvePack) => {
-                const resourceUrl = this.baseurl + `${prefix}.pack`;
-
-                const loadResource = () => {
-                    v86util.load_file(resourceUrl, {
-                        /**
-                         * @param {ArrayBuffer} buffer 
-                         */
-                        done: async buffer => {
-                            this.kv && this.kv.set(resourceUrl, buffer);
-                            resolvePack(buffer);
-                        }
-                    });
-                }
-
-                if (this.kv) {
-                    return this.kv.has(resourceUrl).then(hasCache => {
-                        if (hasCache) {
-                            return resolvePack(this.kv.get(resourceUrl));
-                        }
-                        loadResource();
-                    });
-                }
-                loadResource();
+        // https://jsben.ch/qIzmj
+        const hash = parseInt(sha256sum.substring(0, 8), 16);
+        const fileInfo = this.fileMap.get(hash);
+        // check pack is load status
+        if (!this.packMap[fileInfo.p]) {
+            console.log("[v86][fs] load pack ", fileInfo.p);
+            this.packMap[fileInfo.p] = new Promise((resolvePack) => {
+                const resourceUrl = this.baseurl + `${fileInfo.p.toString(16)}.pack`;
+                fetchArrayBuffer(resourceUrl).then(resolvePack);
             });
         }
-
-        const infoMap = await this.fileInfoMap[prefix];
         /**
          * @type {ArrayBuffer}
          */
-        const pack = await this.packMap[prefix];
+        const pack = await this.packMap[fileInfo.p];
 
-        const hash = sha256sum.split('.')[0];
-
-        return new Uint8Array(pack, infoMap[hash].start, infoMap[hash].end - infoMap[hash].start);
+        return new Uint8Array(pack, fileInfo.s, fileInfo.l);
     }
 
     /**
@@ -522,31 +225,26 @@ class ServerPackStorageWrapper {
      */
     async cache(sha256sum, data) {
         return await this.storage.cache(sha256sum, data);
-    };
+    }
 
     /**
      * @param {string} sha256sum
      */
     uncache(sha256sum) {
         this.storage.uncache(sha256sum);
-    };
+    }
 }
-
-
-
 
 // Closure Compiler's way of exporting
 if (typeof module !== "undefined" && typeof module.exports !== "undefined") {
     module.exports["MemoryFileStorage"] = MemoryFileStorage;
     module.exports["ServerFileStorageWrapper"] = ServerFileStorageWrapper;
     module.exports["ServerPackStorageWrapper"] = ServerPackStorageWrapper;
-}
-else if (typeof window !== "undefined") {
+} else if (typeof window !== "undefined") {
     window["MemoryFileStorage"] = MemoryFileStorage;
     window["ServerFileStorageWrapper"] = ServerFileStorageWrapper;
     window["ServerPackStorageWrapper"] = ServerPackStorageWrapper;
-}
-else if (typeof importScripts === "function") {
+} else if (typeof importScripts === "function") {
     // web worker
     self["MemoryFileStorage"] = MemoryFileStorage;
     self["ServerFileStorageWrapper"] = ServerFileStorageWrapper;
